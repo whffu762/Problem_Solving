9456017f164b3d64afae03e61cf0d4b9fecd22e0
whffu762 <whffu762@naver.com>
2023-09-15 19:10:06 +0900

    0913

--- 깊이_우선_탐색/17484/Main.java
+++ 깊이_우선_탐색/17484/Main.java
@@ -1,104 +1,86 @@
-package pack2;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/*
- * 이동된 좌표에서도 다시 이동될 수 있으므로 깊이 우선 탐색으로 구현
- * 
- * 중복 검사 방지 : 한 번 검사한 좌표는 false 로 설정
- * 중복 카운트 방지 : cal() 함수에 flag 를 넣어서 첫 접근에만 true 로 설정
- * 범위 밖인지 검사 : checkValid()
- * 이동을 순차적으로 진행 : move[] 로 가로 세로 순차적으로 이동 
- * 
- * 재귀 호출을 통해 깊이 우선 탐색
- * 다시 호출될 때는 flag 를 flase 로 설정해서 중복 카운트를 방지
- * 
- */
-class solve1012 {
-    boolean[][] base;
-    int col;
-    int row;
-    int num_target;
-    int[] move = { -1, 1 };
-    int result;
 
-    void input(BufferedReader br) throws IOException {
+/*
+    유효조건 확인
+ * isVaild()로 값이 0과 col 사이에 존재하는지 검사
+ * dir != i 로 이전에 이동한 방향인지 검사 초기값은 -1로 넣어서 해당 조건 무시
+ * 
+ * 
+ * move[] : 이동할 수 있는 방향을 배열로 저장함으로써 모든 이동을 순차적으로 검사할 수 있음
+ * tmp[] : 내가 했던 방식과 마찬가지로 각 행의 열이 저장됨
+ * 
+ * 내가 한 방식과의 차이점은 난 전수 조사로 했지만 
+ * 이 방법은 move[] 를 이용해서 이동 가능 범위만 검사함..
+ */
+class solve17484Dfs{
+    int row;
+    int col;
+    int [][] target;
+    int [] tmp;
+    int result;
+    int [] move = {-1, 0, 1};
+
+    void dfs(int depth, int nowCol, int dir){
+        if(depth == row){   //마지막인지 확인
+            int sum = target[0][tmp[0]];
+            for(int i=1;i<row;i++){
+                sum += target[i][tmp[i]];
+            }
+
+            result = Math.min(result, sum);
+            return;
+        }
+
+        for(int i=0;i<3;i++){
+            int nextCol = nowCol + move[i];
+            if(isValid(nextCol) && dir != i){   //유효조건 확인
+                tmp[depth] = nextCol;
+                dfs(depth+1, nextCol, i);
+            }
+        }
+    }
+
+    boolean isValid(int x){
+        if(x<0 || x >= col){
+            return false;
+        }
+        return true;
+    }
+
+    void run(BufferedReader br) throws IOException{
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        col = Integer.parseInt(st.nextToken());
         row = Integer.parseInt(st.nextToken());
-        num_target = Integer.parseInt(st.nextToken());
+        col = Integer.parseInt(st.nextToken());
 
-        base = new boolean[row][col];
-        for (int j = 0; j < num_target; j++) {
+        target = new int [row][col];
+        for(int i=0;i<row;i++){
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            base[y][x] = true;
-        }
-    }
-
-    void cal(int y, int x, boolean flag) {
-        
-        if (base[y][x]) {
-            base[y][x] = false;
-
-            if(flag){
-                result++;
+            for(int j=0;j<col;j++){
+                target[i][j] = Integer.parseInt(st.nextToken());
             }
-
-            for (int m : move) {
-                int nextCol = x + m;
-                if (checkValid(y, nextCol)) {
-                    cal(y, nextCol, false);
-                }
-            }
-
-            for (int m : move) {
-                int nextRow = y + m;
-                if(checkValid(nextRow, x)){
-                    cal(nextRow, x, false);
-                }
-            }
-
         }
 
-    }
-
-    boolean checkValid(int y, int x) {
-        if (-1 < x && x < col && -1 < y && y < row) {
-            return true;
+        result = row * 100;
+        for(int i=0;i<col;i++){
+            tmp = new int [row];
+            tmp[0] = i;
+            dfs(1, i, -1);
         }
-        return false;
+
+        System.out.println(result);
     }
 
-    void run(BufferedReader br) throws IOException {
-        int num_case = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < num_case; i++) {
-            result = 0;
-            input(br);
-            for (int j = 0; j < row; j++) {
-                for (int k = 0; k < col; k++) {
-                    cal(j, k, true);
-                }
-            }
-
-            System.out.println(result);
-        }
-    }
 }
-
-class Main {
-
-    public static void main(String[] args) throws IOException {
-
+class Main{
+    public static void main(String [] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        solve1012 p = new solve1012();
+        solve17484Dfs p = new solve17484Dfs();
         p.run(br);
 
         br.close();
