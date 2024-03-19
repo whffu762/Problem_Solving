dc8d6dffba814b584e63de05dfac7bd426a07c4b
whffu762 <whffu762@naver.com>
2024-03-19 17:45:56 +0900

    0318

--- 단계별_풀기/9012/Main.java
+++ 단계별_풀기/9012/Main.java
@@ -1,8 +1,7 @@
-package pack2;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 //스택을 이용한 풀이
 class solve9012 {
@@ -49,53 +48,8 @@ class solve9012 {
     }
 }
 
-class solve9012{
+public class Main {
 
-    boolean mine(String target) {
-        int cnt = 0;
-        for(int i=0;i<target.length();i++){
-            if(cnt < 0){
-                return false;
-            }
-
-            if(target.charAt(i) == '('){
-                cnt++;
-            }
-            else{
-                cnt--;
-            }
-        }
-        if(cnt == 0){
-            return true;
-        }
-        else{
-            return false;
-        }
-        
-
-    }
-
-    public void run(BufferedReader br) throws IOException {
-
-        StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < n; i++) {
-            String target = br.readLine();
-            if(mine(target)){
-                sb.append("YES").append("\n");
-            }
-            else{
-                sb.append("NO").append("\n");
-            }
-            
-        }
-
-        System.out.println(sb);
-    }
-}
-
-class Test {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -106,3 +60,5 @@ class Test {
         br.close();
     }
 }
+
+
