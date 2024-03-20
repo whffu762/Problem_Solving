75845267bd10cf69a0eac40b26c0223baeea3899
whffu762 <whffu762@naver.com>
2024-03-20 15:28:08 +0900

    0830

--- 단계별_풀기/9012/Test.java
+++ 단계별_풀기/9012/Test.java
@@ -1,53 +1,6 @@
-package pack2;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
-//스택을 이용한 풀이
-class solve9012 {
-
-    boolean checkPs(String input){
-
-        Stack<Character> stack = new Stack<>();
-        
-        for(int i=0;i<input.length();i++){
-            Character now = input.charAt(i);
-            if(now.equals('(')){
-                stack.push(now);
-            } else {
-                if(stack.empty()){
-                    return false;
-                }
-                stack.pop();
-            }
-        }
-
-        if(stack.empty()){
-            return true;
-        }
-
-        return false;
-    }
-
-
-    void run(BufferedReader br) throws IOException{
-        
-        int num = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-
-        for(int i=0;i<num;i++){
-            String input = br.readLine();
-            if(checkPs(input)){
-                sb.append("YES").append("\n");
-            } else {
-                sb.append("NO").append("\n");
-            }
-        }
-
-        System.out.println(sb);
-    }
-}
 
 class solve9012{
 
