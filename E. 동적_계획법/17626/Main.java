abb1f78d9a73c13caf5a7b2a91734a2068b72c51
whffu762 <whffu762@naver.com>
2024-11-16 02:15:10 +0900

    1115

--- E. 동적_계획법/17626/Main.java
+++ E. 동적_계획법/17626/Main.java
@@ -3,8 +3,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 /*
- * 각 숫자의 최소 제곱근의 갯수를 구해서 최소를 구하는 방식
- * 근데 시간이 너무 오래 걸림 500ms 나오는데 딴거 보니까 100ms 까지 가능..
+ * N까지 필요한 제곱근의 최소를 구하기 위해선 1부터 N-1까지의 최소가 필요함
+ * 예를 들어 13을 구한다고 가정해보자 
+ * 1 : 1을 구할 때 필요한 제곱근의 갯수
+ * 12 : 12를 구할 때 필요한 제곱근의 갯수
+ * 13은 이 둘의 합으로 이루어짐 이런 방식으로 나올 수 있는 경우의 수는 다음과 같음
+ * 1 + 12
+ * 2 + 11
+ * 3 + 10
+ * 4 + 9
+ * 5 + 8
+ * 6 + 7
+ * 이 6가지 중 최소를 고르면 됨 하지만 이 방식은 수가 커질수록 비교 대상도 같이 증가함
+ * N * N / 2 = N ^ 2
+ * 
+ * 시간 줄이기
+ * 생각해보면 최소에는 제곱수가 포함될 수 밖에 없음 제곱근 하나로 구성이 가능하기 때문에
+ * 1 + X의 값이기 때문 그래서 N보다 작은 제곱수들만 뽑아서 비교하면 비교 대상이 몇 안 됨
+ * 10000까지도 100개밖에 안됨 
+ * N * Root(N) = N ^ 1.5
  */
 class solve17626{
 
@@ -36,9 +53,16 @@ class solve17626{
         }
         
         int result = Integer.MAX_VALUE;
-        for(int i=1;i<now/2+1;i++){    
+        // for(int i=1;i<now/2+1;i++){    
 
-            result = Math.min(result, cache[i] + cache[now-i]);
+        //     result = Math.min(result, cache[i] + cache[now-i]);
+        // }
+
+        //개선된 방식
+        for(int i=1;i<(int) Math.sqrt(now)+1;i++){
+
+            int tmp = i * i;
+            result = Math.min(result, cache[tmp] + cache[now-tmp]);
         }
 
         return result;
